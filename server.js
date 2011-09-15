115c4612e982c04d9658e873cf90ef6f220e1010
Evan Prodromou <evan@status.net>
2011-09-15 15:51:08 -0400

    Revert "hello, world version"
    This reverts commit 201033b063666faf2847d70b7244acec15438938.

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
- 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("Hello world\n");
-});
- 
-server.listen(process.env.PORT || 8001);
