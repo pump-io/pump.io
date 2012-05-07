9c3bb1769d153f4c6385510bee9c7cd6284ffb0f
Evan Prodromou <evan@status.net>
2012-05-07 15:23:14 -0400

    better messages for model tests

--- test/lib/model.js
+++ test/lib/model.js
@@ -25,126 +25,131 @@ var assert = require('assert'),
     DatabankObject = databank.DatabankObject;
 
 var modelBatch = function(typeName, className, testSchema, testData) {
+    
+    var batch = {};
+    var typeKey = 'When we require the '+typeName+' module';
+    var classKey = 'and we get its '+className+' class export';
+    var instKey = 'and we create a(n) '+typeName+' instance';
 
-    return {
-        'When we require the module': {
-            topic: function() { 
-                return require('../../lib/model/'+typeName) || null;
+    batch[typeKey] = {
+        topic: function() { 
+            return require('../../lib/model/'+typeName) || null;
+        },
+        'there is one': function(mod) {
+            assert.isObject(mod);
+        },
+        'it has a class export': function(mod) {
+            assert.includes(mod, className);
+        }
+    };
+    
+    batch[typeKey][classKey] = {
+        topic: function(mod) {
+            return mod[className] || null;
+        },
+        'it is a function': function(Cls) {
+            assert.isFunction(Cls);
+        },
+        'it has an init method': function(Cls) {
+            assert.isFunction(Cls.init);
+        },
+        'it has a bank method': function(Cls) {
+            assert.isFunction(Cls.bank);
+        },
+        'it has a get method': function(Cls) {
+            assert.isFunction(Cls.get);
+        },
+        'it has a search method': function(Cls) {
+            assert.isFunction(Cls.search);
+        },
+        'it has a pkey method': function(Cls) {
+            assert.isFunction(Cls.pkey);
+        },
+        'it has a create method': function(Cls) {
+            assert.isFunction(Cls.create);
+        },
+        'it has a readAll method': function(Cls) {
+            assert.isFunction(Cls.readAll);
+        },
+        'its type is correct': function(Cls) {
+            assert.isString(Cls.type);
+            assert.equal(Cls.type, typeName);
+        },
+        'and we get its schema': {
+            topic: function(Cls) {
+                return Cls.schema || null;
             },
-            'there is one': function(mod) {
-                assert.isObject(mod);
+            'it exists': function(schema) {
+                assert.isObject(schema);
             },
-            'it has a class export': function(mod) {
-                assert.includes(mod, className);
+            'it has the right pkey': function(schema) {
+                assert.includes(schema, 'pkey');
+                assert.equal(schema.pkey, testSchema.pkey);
             },
-            'and we get its class export': {
-                topic: function(mod) {
-                    return mod[className] || null;
+            'it has the right fields': function(schema) {
+                var fields = testSchema.fields,
+                    i, field;
+                assert.includes(schema, 'fields');
+                for (i = 0; i < fields.length; i++) {
+                    assert.includes(schema.fields, fields[i]);
+                }
+                for (i = 0; i < schema.fields.length; i++) {
+                    assert.includes(fields, schema.fields[i]);
+                }
+            },
+            'it has the right indices': function(schema) {
+                var fields = testSchema.fields,
+                    i, field;
+                assert.includes(schema, 'fields');
+                for (i = 0; i < fields.length; i++) {
+                    assert.includes(schema.fields, fields[i]);
+                }
+                for (i = 0; i < schema.fields.length; i++) {
+                    assert.includes(fields, schema.fields[i]);
+                }
+            }
+        }
+    };
+
+    batch[typeKey][classKey][instKey] = {
+        topic: function(Cls) {
+            Cls.create(testData.create, this.callback);
+        },
+        'it works correctly': function(err, created) {
+            assert.ifError(err);
+            assert.isObject(created);
+        },
+        'auto-generated fields are there': function(err, created) {
+            assert.isString(created.id);
+            assert.isString(created.published);
+            assert.isString(created.updated); // required for new object?
+        },
+        'passed-in fields are there': function(err, created) {
+            var prop;
+            for (prop in testData.create) {
+                assert.equal(created[prop], testData.create[prop]); 
+            }
+        },
+        'and we modify it': {
+            topic: function(created) {
+                created.update(testData.update, this.callback);
+            },
+            'it is modified': function(err, updated) {
+                assert.ifError(err);
+                assert.isString(updated.updated);
+            },
+            'modified fields are modified': function(err, updated) {
+                var prop;
+                for (prop in testData.create) {
+                    assert.equal(updated[prop], testData.update[prop]); 
+                }
+            },
+            'and we delete it': {
+                topic: function(updated) {
+                    updated.del(this.callback);
                 },
-                'it is a function': function(Cls) {
-                    assert.isFunction(Cls);
-                },
-                'it has an init method': function(Cls) {
-                    assert.isFunction(Cls.init);
-                },
-                'it has a bank method': function(Cls) {
-                    assert.isFunction(Cls.bank);
-                },
-                'it has a get method': function(Cls) {
-                    assert.isFunction(Cls.get);
-                },
-                'it has a search method': function(Cls) {
-                    assert.isFunction(Cls.search);
-                },
-                'it has a pkey method': function(Cls) {
-                    assert.isFunction(Cls.pkey);
-                },
-                'it has a create method': function(Cls) {
-                    assert.isFunction(Cls.create);
-                },
-                'it has a readAll method': function(Cls) {
-                    assert.isFunction(Cls.readAll);
-                },
-                'its type is correct': function(Cls) {
-                    assert.isString(Cls.type);
-                    assert.equal(Cls.type, typeName);
-                },
-                'and we get its schema': {
-                    topic: function(Cls) {
-                        return Cls.schema || null;
-                    },
-                    'it exists': function(schema) {
-                        assert.isObject(schema);
-                    },
-                    'it has the right pkey': function(schema) {
-                        assert.includes(schema, 'pkey');
-                        assert.equal(schema.pkey, testSchema.pkey);
-                    },
-                    'it has the right fields': function(schema) {
-                        var fields = testSchema.fields,
-                            i, field;
-                        assert.includes(schema, 'fields');
-                        for (i = 0; i < fields.length; i++) {
-                            assert.includes(schema.fields, fields[i]);
-                        }
-                        for (i = 0; i < schema.fields.length; i++) {
-                            assert.includes(fields, schema.fields[i]);
-                        }
-                    },
-                    'it has the right indices': function(schema) {
-                        var fields = testSchema.fields,
-                            i, field;
-                        assert.includes(schema, 'fields');
-                        for (i = 0; i < fields.length; i++) {
-                            assert.includes(schema.fields, fields[i]);
-                        }
-                        for (i = 0; i < schema.fields.length; i++) {
-                            assert.includes(fields, schema.fields[i]);
-                        }
-                    }
-                },
-                'and we create an instance': {
-                    topic: function(Cls) {
-                        Cls.create(testData.create, this.callback);
-                    },
-                    'it works correctly': function(err, created) {
-                        assert.ifError(err);
-                        assert.isObject(created);
-                    },
-                    'auto-generated fields are there': function(err, created) {
-                        assert.isString(created.id);
-                        assert.isString(created.published);
-                        assert.isString(created.updated); // required for new object?
-                    },
-                    'passed-in fields are there': function(err, created) {
-                        var prop;
-                        for (prop in testData.create) {
-                            assert.equal(created[prop], testData.create[prop]); 
-                        }
-                    },
-                    'and we modify it': {
-                        topic: function(created) {
-                            created.update(testData.update, this.callback);
-                        },
-                        'it is modified': function(err, updated) {
-                            assert.ifError(err);
-                            assert.isString(updated.updated);
-                        },
-                        'modified fields are modified': function(err, updated) {
-                            var prop;
-                            for (prop in testData.create) {
-                                assert.equal(updated[prop], testData.update[prop]); 
-                            }
-                        },
-                        'and we delete it': {
-                            topic: function(updated) {
-                                updated.del(this.callback);
-                            },
-                            'it works': function(err, updated) {
-                                assert.ifError(err);
-                            }
-                        }
-                    }
+                'it works': function(err, updated) {
+                    assert.ifError(err);
                 }
             }
         }
