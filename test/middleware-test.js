0fb228290473fbe26679fd115df73868396d06f4
Evan Prodromou <evan@status.net>
2012-05-18 09:03:29 -0400

    Test module for middleware

--- test/middleware-test.js
+++ test/middleware-test.js
@@ -17,16 +17,57 @@
 // limitations under the License.
 
 var assert = require('assert'),
-    vows = require('vows');
+    vows = require('vows'),
+    databank = require('databank'),
+    Step = require('step'),
+    schema = require('../lib/schema'),
+    URLMaker = require('../lib/urlmaker').URLMaker,
+    randomString = require('../lib/randomstring').randomString,
+    Client = require('../lib/model/client').Client,
+    RequestToken = require('../lib/model/requesttoken').RequestToken,
+    AccessToken = require('../lib/model/accesstoken').AccessToken,
+    User = require('../lib/model/user').User,
+    methodContext = require('./lib/methods').methodContext,
+    Databank = databank.Databank,
+    DatabankObject = databank.DatabankObject;
 
 vows.describe('middleware module interface').addBatch({
-    'When we check for a test suite': {
+
+    'When we load the module': {
+
         topic: function() { 
-            return false;
+
+            var cb = this.callback;
+            // Need this to make IDs
+
+            URLMaker.hostname = "example.net";
+
+            // Dummy databank
+
+            var params = {schema: schema};
+
+            var db = Databank.get('memory', params);
+
+            db.connect({}, function(err) {
+                var mod;
+
+                DatabankObject.bank = db;
+
+                mod = require('../lib/provider');
+                cb(null, mod);
+            });
         },
-        'there is one': function(tsExists) {
-            assert.isTrue(tsExists);
-        }
+        'there is one': function(mod) {
+            assert.isObject(mod);
+        },
+        'and we check its methods': methodContext(['maybeAuth',
+                                                   'reqUser',
+                                                   'mustAuth',
+                                                   'sameUser',
+                                                   'noUser',
+                                                   'checkCredentials',
+                                                   'getCurrentUser',
+                                                   'getSessionUser'])
     }
 }).export(module);
 
