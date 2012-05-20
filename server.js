b6b0b1d0b65dada058b639a7bbf375ad7b0c4c38
Evan Prodromou <evan@status.net>
2012-05-20 09:06:45 -0400

    New entry point
    server.js is an entry point that loads the local config file and
    starts the app.

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+//
+// entry point activity pump application
+//
+// Copyright 2011-2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var makeApp = require('./lib/app').makeApp;
+var config = require('./config');
+
+makeApp(config, function(err, app) {
+    if (err) {
+        console.log(err);
+    } else {
+        app.run();
+    }
+});
+
