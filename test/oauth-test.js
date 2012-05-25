f89f45703a1fc8d2d3a48ff2914295f3d3362c02
Evan Prodromou <evan@status.net>
2012-05-25 10:30:50 -0400

    test oauth endpoints

---
+++ test/oauth-test.js
@@ -0,0 +1,72 @@
+// oauth-test.js
+//
+// Test the client registration API
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require('assert'),
+    vows = require('vows'),
+    Step = require('step'),
+    _ = require('underscore'),
+    querystring = require('querystring'),
+    OAuth = require('oauth').OAuth,
+    httputil = require('./lib/http');
+
+var suite = vows.describe('user API');
+
+suite.addBatch({
+    'When we set up the app': {
+        topic: function() {
+            var cb = this.callback,
+                config = {port: 4815,
+                          hostname: 'localhost',
+                          driver: 'memory',
+                          params: {},
+                          nologger: true
+                         },
+                makeApp = require('../lib/app').makeApp;
+
+            process.env.NODE_ENV = 'test';
+
+            makeApp(config, function(err, app) {
+                if (err) {
+                    cb(err, null);
+                } else {
+                    app.run(function(err) {
+                        if (err) {
+                            cb(err, null);
+                        } else {
+                            cb(null, app);
+                        }
+                    });
+                }
+            });
+        },
+        teardown: function(app) {
+            app.close();
+        },
+        'it works': function(err, app) {
+            assert.ifError(err);
+        },
+        'and we check the request token endpoint': 
+        httputil.endpoint('/oauth/request_token', ['GET', 'POST']),
+        'and we check the access token endpoint':
+        httputil.endpoint('/oauth/authorize', ['GET', 'POST']),
+        'and we check the authorization endpoint':
+        httputil.endpoint('/oauth/access_token', ['GET', 'POST'])
+    }
+});
+
+suite['export'](module);
