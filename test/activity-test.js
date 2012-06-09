5fa1d189e0aa1bf9d922d11052cb172341932403
Evan Prodromou <evan@status.net>
2012-06-09 10:23:45 -0400

    check that it's the right value in the lists

--- test/activity-test.js
+++ test/activity-test.js
@@ -396,10 +396,12 @@ suite.addBatch({
                 'following list is correct': function(err, res) {
                     assert.isArray(res.following);
                     assert.lengthOf(res.following, 1);
+                    assert.equal(res.following[0], res.users.bob.profile.id);
                 },
                 'followers list is correct': function(err, res) {
                     assert.isArray(res.followers);
                     assert.lengthOf(res.followers, 1);
+                    assert.equal(res.followers[0], res.users.alice.profile.id);
                 },
                 'and we apply() a stop-following activity': {
                     topic: function(res, users, Activity) {
