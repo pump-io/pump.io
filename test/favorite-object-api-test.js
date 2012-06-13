666868f8ffa335cd20f48d471a3b881e27912a34
Evan Prodromou <evan@status.net>
2012-06-13 14:54:27 -0400

    Didn't pass down pairs

--- test/favorite-object-api-test.js
+++ test/favorite-object-api-test.js
@@ -373,7 +373,7 @@ suite.addBatch({
                             httputil.postJSON(url, cred, act, this);
                         },
                         function(err, doc, response) {
-                            cb(err, doc);
+                            cb(err, doc, pairs);
                         }
                     );
                 },
