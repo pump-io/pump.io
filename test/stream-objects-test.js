6d1b169bc9e2d88e52c0de5aeaa6ee65a358b955
Evan Prodromou <evan@status.net>
2012-07-07 16:04:51 -0400

    Deliver a bunch of objects to the stream

--- test/stream-objects-test.js
+++ test/stream-objects-test.js
@@ -175,7 +175,7 @@ suite.addBatch({
             topic: function(Stream) { 
                 Stream.create({name: "object-test-2"}, this.callback);
             },
-            "it works": function(err, stream, Stream) {
+            "it works": function(err, stream) {
                 assert.ifError(err);
                 assert.isObject(stream);
             },
@@ -208,6 +208,61 @@ suite.addBatch({
                         assert.equal(objects[0].objectType, "person");
                         assert.include(objects[0], "id");
                         assert.equal(objects[0].id, "acct:evan@status.net");
+                    }
+                }
+            }
+        },
+        "and we create a stream and deliver many objects to it": {
+            topic: function(Stream) {
+                var cb = this.callback,
+                    stream;
+
+                Step(
+                    function() {
+                        Stream.create({name: "object-test-3"}, this);
+                    },
+                    function(err, results) {
+                        var i, group = this.group();
+                        if (err) throw err;
+                        stream = results;
+                        for (i = 0; i < 50; i++) {
+                            stream.deliverObject({id: "http://example.com/person" + i, objectType: "person"}, group());
+                        }
+                    },
+                    function(err) {
+                        if (err) {
+                            cb(err, null);
+                        } else {
+                            cb(null, stream);
+                        }
+                    }
+                );
+            },
+            "it works": function(err, stream) {
+                assert.ifError(err);
+                assert.isObject(stream);
+            },
+            "and we get the stream's objects": {
+                topic: function(stream) {
+                    stream.getObjects(0, 100, this.callback);
+                },
+                "it works": function(err, objects) {
+                    assert.ifError(err);
+                },
+                "results look right": function(err, objects) {
+                    var i, obj, seen = {};
+                    assert.ifError(err);
+                    assert.isArray(objects);
+                    assert.lengthOf(objects, 50);
+                    for (i = 0; i < objects.length, i++) {
+                        obj = objects[i];
+                        assert.isObject(obj);
+                        assert.include(obj, "objectType");
+                        assert.equal(obj.objectType, "person");
+                        assert.include(obj, "id");
+                        assert.match(obj.id, /http:\/\/example.com\/[0-9]+/);
+                        assert.isUndefined(seen[obj.id]);
+                        seen[obj.id] = obj;
                     }
                 }
             }
