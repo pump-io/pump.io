8bb676770422539eb079ad6e55b92659dda86ede
Evan Prodromou <evan@status.net>
2012-07-31 11:49:10 -0400

    replies API test

---
+++ test/replies-api-test.js
@@ -0,0 +1,98 @@
+// activity-api-test.js
+//
+// Test activity REST API
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert"),
+    vows = require("vows"),
+    Step = require("step"),
+    _ = require("underscore"),
+    http = require("http"),
+    urlparse = require("url").parse,
+    httputil = require("./lib/http"),
+    oauthutil = require("./lib/oauth"),
+    actutil = require("./lib/activity"),
+    setupApp = oauthutil.setupApp,
+    register = oauthutil.register,
+    accessToken = oauthutil.accessToken,
+    newCredentials = oauthutil.newCredentials;
+
+var suite = vows.describe("Activity API test");
+
+// A batch for testing the read-write access to the API
+
+suite.addBatch({
+    "When we set up the app": {
+        topic: function() {
+            setupApp(this.callback);
+        },
+        teardown: function(app) {
+            if (app && app.close) {
+                app.close();
+            }
+        },
+        "it works": function(err, app) {
+            assert.ifError(err);
+        },
+        "and we get new credentials": {
+            topic: function() {
+                newCredentials("macdonald", "theoldflag", this.callback);
+            },
+            "it works": function(err, cred) {
+                assert.ifError(err);
+                assert.isObject(cred);
+                assert.isString(cred.consumer_key);
+                assert.isString(cred.consumer_secret);
+                assert.isString(cred.token);
+                assert.isString(cred.token_secret);
+            },
+            "and we post a new activity": {
+                topic: function(cred) {
+                    var cb = this.callback,
+                        act = {
+                            verb: "post",
+                            object: {
+                                objectType: "note",
+                                content: "The people would prefer John A. drunk to George Brown sober."
+                            }
+                        };
+                    httputil.postJSON("http://localhost:4815/api/user/macdonald/feed", cred, act, function(err, act, response) {
+                        cb(err, act);
+                    });
+                },
+                "it works": function(err, act) {
+                    assert.ifError(err);
+                    assert.isObject(act);
+                },
+                "the object includes a replies property": function(err, act) {
+                    assert.ifError(err);
+                    assert.isObject(act);
+                    assert.includes(act, "object");
+                    assert.isObject(act.object);
+                    assert.includes(act.object, "replies");
+                    assert.isObject(act.object.replies);
+                    assert.includes(act.object.replies, "url");
+                    assert.isString(act.object.replies.url);
+                    assert.includes(act.object.replies, "totalItems");
+                    assert.isNumber(act.object.replies.totalItems);
+                    assert.equal(act.object.replies.totalItems, 0);
+                }
+            }
+        }
+    }
+});
+
+suite["export"](module);
