344284cddb6b6537ec0d616b27b2aef909d3dc8f
Evan Prodromou <evan@status.net>
2012-08-22 13:40:02 -0400

    Add new application object type
    Closes #32.

--- test/activityobject-test.js
+++ test/activityobject-test.js
@@ -100,6 +100,7 @@ vows.describe("activityobject class interface").addBatch({
             },
             "it has constant-ish members for known types": function(ActivityObject) {
                 assert.equal(ActivityObject.ALERT, "alert");
+                assert.equal(ActivityObject.APPLICATION, "application");
                 assert.equal(ActivityObject.ARTICLE, "article");
                 assert.equal(ActivityObject.AUDIO, "audio");
                 assert.equal(ActivityObject.BADGE, "badge");
