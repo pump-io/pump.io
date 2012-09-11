5080b3faba6d106196ac73ae0c808fb7ac700b33
Evan Prodromou <evan@status.net>
2012-09-11 14:46:28 -0400

    Remove logging entirely.

--- test/lib/dialback.js
+++ test/lib/dialback.js
@@ -25,7 +25,6 @@ var dialbackApp = function(port, hostname, callback) {
     app.configure(function(){
         app.set("port", port);
         app.use(express.favicon());
-        app.use(express.logger());
         app.use(express.bodyParser());
         app.use(express.methodOverride());
         app.use(app.router);
