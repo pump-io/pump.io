7e94dc62822391c825b447c61f02167165fcdb87
Evan Prodromou <evan@status.net>
2012-09-17 11:25:05 -0400

    Endpoint for checking dialback data
    An HTTP endpoint for verifying dialback requests.

---
+++ test/dialback-route-test.js
@@ -0,0 +1,47 @@
+// dialback-route-test.js
+//
+// Test the dialback route for the app
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert"),
+    vows = require("vows"),
+    Step = require("step"),
+    _ = require("underscore"),
+    httputil = require("./lib/http"),
+    oauthutil = require("./lib/oauth"),
+    setupApp = oauthutil.setupApp;
+
+var ignore = function(err) {};
+
+var suite = vows.describe("dialback endpoint API");
+
+suite.addBatch({
+    "When we set up the app": {
+        topic: function() {
+            setupApp(this.callback);
+        },
+        teardown: function(app) {
+            app.close();
+        },
+        "it works": function(err, app) {
+            assert.ifError(err);
+        },
+        "and we check the dialback endpoint": 
+        httputil.endpoint("/api/dialback", ["POST"])
+    }
+});
+
+suite["export"](module);
