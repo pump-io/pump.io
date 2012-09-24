5985905c44ba166181d2bd4e4cbc9e51c4b3ae0c
Evan Prodromou <evan@status.net>
2012-09-24 17:34:19 -0400

    Optionally redirect all HTTP traffic to HTTPS
    For all HTTP traffic, redirect it to HTTPS.

---
+++ test/app-bounce-test-as-root.js
@@ -0,0 +1,104 @@
+// app-https-test-as-root.js
+//
+// Test running the app over HTTPS
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert"),
+    vows = require("vows"),
+    fs = require("fs"),
+    path = require("path"),
+    databank = require("databank"),
+    Step = require("step"),
+    http = require("http"),
+    https = require("https"),
+    urlparse = require("url").parse,
+    httputil = require("./lib/http"),
+    oauthutil = require("./lib/oauth"),
+    xrdutil = require("./lib/xrd");
+
+var suite = vows.describe("bounce 80 to 443 app interface");
+
+suite.addBatch({
+    "When we makeApp()": {
+        topic: function() {
+            var config = {port: 443,
+                          hostname: "social.localhost",
+                          key: path.join(__dirname, "data", "social.localhost.key"),
+                          cert: path.join(__dirname, "data", "social.localhost.crt"),
+                          driver: "memory",
+                          params: {},
+                          nologger: true,
+                          bounce: true
+                         },
+                makeApp = require("../lib/app").makeApp;
+
+            process.env.NODE_ENV = "test";
+
+            makeApp(config, this.callback);
+        },
+        "it works": function(err, app) {
+            assert.ifError(err);
+            assert.isObject(app);
+        },
+        "and we app.run()": {
+            topic: function(app) {
+                var cb = this.callback;
+                app.run(function(err) {
+                    if (err) {
+                        cb(err, null);
+                    } else {
+                        cb(null, app);
+                    }
+                });
+            },
+            teardown: function(app) {
+                if (app && app.close) {
+                    app.close();
+                }
+            },
+            "it works": function(err, app) {
+                assert.ifError(err);
+            },
+            "app is listening on correct port": function(err, app) {
+                var addr = app.address();
+                assert.equal(addr.port, 443);
+            },
+            "and we GET the host-meta file": {
+                topic: function() {
+                    var callback = this.callback,
+                        req;
+
+                    req = http.get("http://social.localhost/.well-known/host-meta", function(res) {
+                        callback(null, res);
+                    });
+                    req.on("error", function(err) {
+                        callback(err, null);
+                    });
+                },
+                "it works": function(err, res) {
+                    assert.ifError(err);
+                },
+                "it redirects to the HTTPS version": function(err, res) {
+                    assert.ifError(err);
+                    assert.equal(res.statusCode, 301);
+                    assert.equal(res.headers.location, "https://social.localhost/.well-known/host-meta");
+                }
+            }
+        }
+    }
+});
+
+suite["export"](module);
