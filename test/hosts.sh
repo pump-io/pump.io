7a15d7cd4b9cb14e1dd7f5e91603ad91454e38a3
Evan Prodromou <evan@status.net>
2012-09-25 09:54:23 -0400

    add secure.localhost

--- test/hosts.sh
+++ test/hosts.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-grep "dialback.localhost" /etc/hosts || echo "127.0.69.1 dialback.localhost" | sudo tee -a /etc/hosts
-grep "social.localhost" /etc/hosts || echo "127.0.69.2 social.localhost" | sudo tee -a /etc/hosts
-grep "photo.localhost" /etc/hosts || echo "127.0.69.3 photo.localhost" | sudo tee -a /etc/hosts
-grep "echo.localhost" /etc/hosts || echo "127.0.69.4 echo.localhost" | sudo tee -a /etc/hosts
+grep "dialback.localhost" /etc/hosts || echo "127.0.69.1 dialback.localhost dialback" | sudo tee -a /etc/hosts
+grep "social.localhost" /etc/hosts || echo "127.0.69.2 social.localhost social" | sudo tee -a /etc/hosts
+grep "photo.localhost" /etc/hosts || echo "127.0.69.3 photo.localhost photo" | sudo tee -a /etc/hosts
+grep "echo.localhost" /etc/hosts || echo "127.0.69.4 echo.localhost echo" | sudo tee -a /etc/hosts
+grep "secure.localhost" /etc/hosts || echo "127.0.69.5 secure.localhost secure" | sudo tee -a /etc/hosts
+
