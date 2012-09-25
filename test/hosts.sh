5ab9f3a9ef5ee6d34132a71b540c4be0aa67965d
Evan Prodromou <evan@status.net>
2012-09-25 10:16:23 -0400

    Add bounce server

--- test/hosts.sh
+++ test/hosts.sh
@@ -5,4 +5,6 @@ grep "social.localhost" /etc/hosts || echo "127.0.69.2 social.localhost social" 
 grep "photo.localhost" /etc/hosts || echo "127.0.69.3 photo.localhost photo" | sudo tee -a /etc/hosts
 grep "echo.localhost" /etc/hosts || echo "127.0.69.4 echo.localhost echo" | sudo tee -a /etc/hosts
 grep "secure.localhost" /etc/hosts || echo "127.0.69.5 secure.localhost secure" | sudo tee -a /etc/hosts
+grep "bounce.localhost" /etc/hosts || echo "127.0.69.6 bounce.localhost bounce" | sudo tee -a /etc/hosts
+
 
