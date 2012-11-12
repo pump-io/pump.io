5acaef19c8fbb664b8697421b10b15c0ebdad329
Evan Prodromou <evan@status.net>
2012-11-12 12:58:41 -0500

    Enable spinner

---
+++ public/javascript/jquery-spin.js
@@ -0,0 +1,18 @@
+(function($, Spinner) {
+    $.fn.spin = function(opts) {
+        this.each(function() {
+            var $this = $(this),
+                data = $this.data();
+
+            if (data.spinner) {
+                data.spinner.stop();
+                delete data.spinner;
+            }
+            if (opts !== false) {
+                data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+            }
+        });
+        return this;
+    };
+})(window.jQuery, window.Spinner);
+
