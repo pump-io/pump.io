e8e39202865d2c5882d30d82897ad687a3373eae
Evan Prodromou <evan@status.net>
2012-11-13 21:12:07 -0500

    Fixup some numbers for new welcome and registration activities

--- test/distributor-remote-test-as-root.js
+++ test/distributor-remote-test-as-root.js
@@ -232,7 +232,7 @@ suite.addBatch({
                             assert.isObject(act);
                             assert.include(feed, "items");
                             assert.isArray(feed.items);
-                            assert.lengthOf(feed.items, 1);
+                            assert.lengthOf(feed.items, 3);
                             assert.isObject(feed.items[0]);
                             assert.include(feed.items[0], "id");
                             assert.equal(feed.items[0].id, act.id);
@@ -293,7 +293,7 @@ suite.addBatch({
                                     assert.isObject(act);
                                     assert.include(feed, "items");
                                     assert.isArray(feed.items);
-                                    assert.lengthOf(feed.items, 2);
+                                    assert.lengthOf(feed.items, 4);
                                     assert.isObject(feed.items[0]);
                                     assert.include(feed.items[0], "id");
                                     assert.equal(feed.items[0].id, act.id);
@@ -302,7 +302,7 @@ suite.addBatch({
                                     assert.ifError(err);
                                     assert.isObject(feed);
                                     assert.isArray(feed.items);
-                                    assert.lengthOf(feed.items, 2);
+                                    assert.lengthOf(feed.items, 4);
                                     assert.isObject(feed.items[0]);
                                     assert.isObject(feed.items[0].actor);
                                     assert.isFalse(_(feed.items[0].actor).has("_user"));
@@ -311,7 +311,7 @@ suite.addBatch({
                                     assert.ifError(err);
                                     assert.isObject(feed);
                                     assert.isArray(feed.items);
-                                    assert.lengthOf(feed.items, 2);
+                                    assert.lengthOf(feed.items, 4);
                                     assert.isObject(feed.items[0]);
                                     assert.isObject(feed.items[0].object);
                                     assert.isObject(feed.items[0].object.likes);
@@ -379,7 +379,7 @@ suite.addBatch({
                                             assert.isObject(act);
                                             assert.include(feed, "items");
                                             assert.isArray(feed.items);
-                                            assert.lengthOf(feed.items, 3);
+                                            assert.lengthOf(feed.items, 5);
                                             assert.isObject(feed.items[0]);
                                             assert.include(feed.items[0], "id");
                                             assert.equal(feed.items[0].id, act.id);
@@ -466,7 +466,7 @@ suite.addBatch({
                                                     assert.isObject(act);
                                                     assert.include(feed, "items");
                                                     assert.isArray(feed.items);
-                                                    assert.lengthOf(feed.items, 4);
+                                                    assert.lengthOf(feed.items, 6);
                                                     assert.isObject(feed.items[0]);
                                                     assert.include(feed.items[0], "id");
                                                     assert.equal(feed.items[0].id, act.id);
