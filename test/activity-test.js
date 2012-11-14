2d120cdef2590ff7652d0e3f1d239af8c73320ce
Evan Prodromou <evan@status.net>
2012-11-13 23:42:45 -0500

    automatically generate activity content

--- test/activity-test.js
+++ test/activity-test.js
@@ -1376,4 +1376,150 @@ suite.addBatch({
     }
 });
 
+var contentCheck = function(actor, verb, object, expected) {
+    return {
+        topic: function(Activity) {
+            var callback = this.callback,
+                Collection = require("../lib/model/collection").Collection,
+                thePublic = {
+                    objectType: "collection",
+                    id: Collection.PUBLIC
+                },
+                act = new Activity({actor: actor,
+                                    to: [thePublic],
+                                    verb: verb,
+                                    object: object});
+            
+            Step(
+                function() {
+                    // First, ensure recipients
+                    act.ensureRecipients(this);
+                },
+                function(err) {
+                    if (err) throw err;
+                    // Then, apply the activity
+                    act.apply(null, this);
+                },
+                function(err) {
+                    if (err) throw err;
+                    // Then, save the activity
+                    Activity.create(act, this);
+                },
+                function(err, saved) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        callback(null, act);
+                    }
+                }
+            );
+        },
+        "it works": function(err, act) {
+            assert.ifError(err);
+        },
+        "content is correct": function(err, act) {
+            assert.ifError(err);
+            assert.include(act, "content");
+            assert.isString(act.content);
+            assert.equal(act.content, expected);
+        }
+    };
+};
+
+suite.addBatch({
+    "When we get the Activity class": {
+        topic: function() {
+            var cb = this.callback;
+            // Need this to make IDs
+
+            URLMaker.hostname = "example.net";
+
+            // Dummy databank
+
+            tc.params.schema = schema;
+
+            var db = Databank.get(tc.driver, tc.params);
+
+            db.connect({}, function(err) {
+
+                var mod;
+
+                if (err) {
+                    cb(err, null);
+                    return;
+                }
+
+                DatabankObject.bank = db;
+                
+                mod = require("../lib/model/activity");
+
+                if (!mod) {
+                    cb(new Error("No module"), null);
+                    return;
+                }
+
+                cb(null, mod.Activity);
+            });
+        },
+        "it works": function(err, Activity) {
+            assert.ifError(err);
+            assert.isFunction(Activity);
+        },
+        "and a person with no url posts a note":
+        contentCheck({objectType: "person",
+                      id: "cbf1f0ca-2e10-11e2-8174-70f1a154e1aa",
+                      displayName: "Jessie Belnap"},
+                     "post",
+                     {objectType: "note",
+                      id: "f4f859a0-2e10-11e2-9af6-70f1a154e1aa",
+                      content: "Hello, world."},
+                     "Jessie Belnap posted a note"),
+        "and a person with an url posts a note":
+        contentCheck({objectType: "person",
+                      id: "e67c969c-2e11-11e2-86dc-70f1a154e1aa",
+                      url: "http://malloryfellman.example/",
+                      displayName: "Mallory Fellman"},
+                     "post",
+                     {objectType: "note",
+                      id: "a453c0fa-2e12-11e2-9214-70f1a154e1aa",
+                      content: "Hello, world."},
+                     "<a href='http://malloryfellman.example/'>Mallory Fellman</a> posted a note"),
+        "and a person with no url plays a video":
+        contentCheck({objectType: "person",
+                      id: "6958bee6-2e13-11e2-be89-70f1a154e1aa",
+                      displayName: "Mathew Penniman"},
+                     "play",
+                     {objectType: "video",
+                      url: "http://www.youtube.com/watch?v=J---aiyznGQ",
+                      displayName: "Keyboard Cat"},
+                     "Mathew Penniman played <a href='http://www.youtube.com/watch?v=J---aiyznGQ'>Keyboard Cat</a>"),
+        "and a person with no url closes a file":
+        contentCheck({objectType: "person",
+                      id: "5abbfc3a-2e14-11e2-b27c-70f1a154e1aa",
+                      displayName: "Clayton Barto"},
+                     "close",
+                     {objectType: "file",
+                      id: "8e5b739a-2e14-11e2-ab8e-70f1a154e1aa"},
+                     "Clayton Barto closed a file"),
+        "and a person posts a comment in reply to an image":
+        contentCheck({objectType: "person",
+                      id: "dc3c3572-2e14-11e2-91fc-70f1a154e1aa",
+                      displayName: "Lorrie Tynan"},
+                     "post",
+                     {objectType: "comment",
+                      id: "f58da61e-2e14-11e2-a207-70f1a154e1aa",
+                      inReplyTo: {
+                          objectType: "image",
+                          id: "0d61105a-2e15-11e2-b6b4-70f1a154e1aa",
+                          author: {
+                              displayName: "Karina Cosenza",
+                              id: "259e793c-2e15-11e2-b437-70f1a154e1aa",
+                              objectType: "person"
+                          }
+                      }
+                     },
+                     "Lorrie Tynan posted a comment in reply to an image")
+    }
+});
+
 suite["export"](module);
