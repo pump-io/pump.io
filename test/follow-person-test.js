6ee86a6d52551580904bff8cf1f6594c1c0ba8a9
Evan Prodromou <evan@status.net>
2012-11-13 20:21:00 -0500

    Fixup numbers for follow-person-test

--- test/follow-person-test.js
+++ test/follow-person-test.js
@@ -317,10 +317,10 @@ suite.addBatch({
                     assert.isObject(inbox);
                     assert.include(inbox, "totalItems");
                     assert.isNumber(inbox.totalItems);
-                    assert.equal(inbox.totalItems, 2);
+                    assert.equal(inbox.totalItems, 4);
                     assert.include(inbox, "items");
                     assert.isArray(inbox.items);
-                    assert.lengthOf(inbox.items, 2);
+                    assert.lengthOf(inbox.items, 4);
                     assert.isObject(inbox.items[0]);
                     assert.include(inbox.items[0], "id");
                     assert.isObject(postnote);
@@ -410,7 +410,7 @@ suite.addBatch({
                         assert.isObject(feed);
                         assert.include(feed, "items");
                         assert.isArray(feed.items);
-                        assert.lengthOf(feed.items, 1);
+                        assert.lengthOf(feed.items, 2);
                         assert.isObject(feed.items[0]);
                         assert.include(feed.items[0], "verb");
                         assert.equal("follow", feed.items[0].verb);
