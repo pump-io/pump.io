3187a7c2bb0d9e88f5b5f6ac6abdddd2c96d2c2e
Evan Prodromou <evan@status.net>
2012-12-05 05:39:40 -0500

    Make initial virtual collections for user followers, following
    Closes #239

---
+++ test/user-virtual-collection-test.js
@@ -0,0 +1,142 @@
+// user-virtual-collection-test.js
+//
+// Test the followers, following collections for a new user
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert"),
+    vows = require("vows"),
+    fs = require("fs"),
+    path = require("path"),
+    databank = require("databank"),
+    _ = require("underscore"),
+    Step = require("step"),
+    Collection = require("../lib/model/collection").Collection,
+    URLMaker = require("../lib/urlmaker").URLMaker,
+    schema = require('../lib/schema').schema,
+    Databank = databank.Databank,
+    DatabankObject = databank.DatabankObject;
+
+var suite = vows.describe("user virtual collection interface");
+
+var tc = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")));
+
+suite.addBatch({
+    "When we get the User class": {
+        topic: function() { 
+
+            var cb = this.callback;
+            // Need this to make IDs
+
+            URLMaker.hostname = "example.net";
+
+            // Dummy databank
+
+            tc.params.schema = schema;
+
+            var db = Databank.get(tc.driver, tc.params);
+
+            db.connect({}, function(err) {
+                var User;
+
+                DatabankObject.bank = db;
+                
+                User = require("../lib/model/user").User || null;
+
+                cb(null, User);
+            });
+        },
+        "and we create a user": {
+            topic: function(User) {
+                var props = {
+                    nickname: "jared",
+                    password: "eeng7Dox"
+                };
+                User.create(props, this.callback);
+            },
+            "it works": function(err, user) {
+                assert.ifError(err);
+                assert.isObject(user);
+            },
+            teardown: function(user) {
+                if (user && user.del) {
+                    user.del(function(err) {});
+                }
+            },
+            "and we get the followers collection": {
+                topic: function(user) {
+                    var id = "http://example.net/api/"+user.nickname+"/followers",
+                        callback = this.callback;
+
+                    Collection.get(id, callback);
+                },
+                "it works": function(err, coll) {
+                    assert.ifError(err);
+                    assert.isObject(coll);
+                },
+                "it has the right members": function(err, coll) {
+                    assert.ifError(err);
+                    assert.isObject(coll);
+                    assert.equal(coll.id, "http://example.net/api/jared/followers");
+                    assert.equal(coll.url, "http://example.net/jared/followers");
+                    assert.equal(coll.displayName, "Followers");
+                    assert.equal(coll.links.self.href, "http://example.net/api/jared/followers");
+                    assert.equal(coll.members.url, "http://example.net/api/jared/followers");
+                },
+                "and we check if it's a list": {
+                    topic: function(coll) {
+                        Collection.isList(coll, this.callback);
+                    },
+                    "it is not": function(err, user) {
+                        assert.ifError(err);
+                        assert.isFalse(user);
+                    }
+                }
+            },
+            "and we get the following collection": {
+                topic: function(user) {
+                    var id = "http://example.net/api/"+user.nickname+"/following",
+                        callback = this.callback;
+
+                    Collection.get(id, callback);
+                },
+                "it works": function(err, coll) {
+                    assert.ifError(err);
+                    assert.isObject(coll);
+                },
+                "it has the right members": function(err, coll) {
+                    assert.ifError(err);
+                    assert.isObject(coll);
+                    assert.equal(coll.id, "http://example.net/api/jared/following");
+                    assert.equal(coll.url, "http://example.net/jared/following");
+                    assert.equal(coll.displayName, "Following");
+                    assert.equal(coll.links.self.href, "http://example.net/api/jared/following");
+                    assert.equal(coll.members.url, "http://example.net/api/jared/following");
+                },
+                "and we check if it's a list": {
+                    topic: function(coll) {
+                        Collection.isList(coll, this.callback);
+                    },
+                    "it is not": function(err, user) {
+                        assert.ifError(err);
+                        assert.isFalse(user);
+                    }
+                }
+            }
+        }
+    }
+});
+
+suite["export"](module);
