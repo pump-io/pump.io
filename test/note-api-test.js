19eda34171aad4f0777bcffc117e284c7fdaa6ee
Evan Prodromou <evan@status.net>
2012-12-11 18:21:52 -0500

    Don't expect magic numbers in feed for note api test

--- test/note-api-test.js
+++ test/note-api-test.js
@@ -405,7 +405,7 @@ suite.addBatch({
                         assert.isObject(feed);
                         assert.include(feed, "items");
                         assert.isArray(feed.items);
-                        assert.lengthOf(feed.items, 3);
+                        assert.greater(feed.items.length, 0);
                         assert.isObject(feed.items[0]);
                         assert.include(feed.items[0], "verb");
                         assert.equal("update", feed.items[0].verb);
@@ -490,7 +490,7 @@ suite.addBatch({
                         assert.isObject(feed);
                         assert.include(feed, "items");
                         assert.isArray(feed.items);
-                        assert.lengthOf(feed.items, 3);
+                        assert.greater(feed.items.length, 0);
                         assert.isObject(feed.items[0]);
                         assert.include(feed.items[0], "verb");
                         assert.equal("delete", feed.items[0].verb);
