e914135567052ca3ef6ae6d15ec6700c9f68052c
Evan Prodromou <evan@status.net>
2012-12-26 06:13:09 -0800

    Added dispatch test

---
+++ test/dispatch-test.js
@@ -0,0 +1,43 @@
+// dispatch-test.js
+//
+// Test for dispatch module
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert"),
+    vows = require("vows");
+
+var suite = vows.describe("dispatch module interface");
+
+suite.addBatch({
+    "When we require the dispatch module": {
+        topic: function() {
+            return require("../lib/dispatch");
+        },
+        "it returns an object": function(Dispatch) {
+            assert.isObject(Dispatch);
+        },
+        "and we check its methods": {
+            topic: function(Dispatch) {
+                return Dispatch;
+            },
+            "it has a start method": function(Dispatch) {
+                assert.isFunction(Dispatch.start);
+            }
+        }
+    }
+});
+
+suite["export"](module);
