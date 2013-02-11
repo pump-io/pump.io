e9b117593f69846309b0b418a17883ca89edb5d0
Evan Prodromou <evan@status.net>
2013-02-11 14:53:07 -0500

    Test host discovery

---
+++ test/host-test-as-root.js
@@ -0,0 +1,194 @@
+// host-test-as-root.js
+//
+// Online test of the Host module
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert"),
+    vows = require("vows"),
+    Step = require("step"),
+    querystring = require("querystring"),
+    _ = require("underscore"),
+    fs = require("fs"),
+    path = require("path"),
+    express = require("express"),
+    URLMaker = require("../lib/urlmaker").URLMaker,
+    DialbackClient = require("dialback-client"),
+    databank = require("databank"),
+    Databank = databank.Databank,
+    DatabankObject = databank.DatabankObject,
+    Host = require("../lib/model/host").Host,
+    httputil = require("./lib/http"),
+    oauthutil = require("./lib/oauth"),
+    setupAppConfig = oauthutil.setupAppConfig;
+
+var suite = vows.describe("host module interface");
+
+var tc = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")));
+
+var tinyApp = function(port, hostname, callback) {
+
+    var app = express.createServer();
+
+    app.configure(function(){
+        app.set("port", port);
+        app.use(express.bodyParser());
+        app.use(app.router);
+    });
+
+    app.get("/.well-known/host-meta.json", function(req, res) {
+        res.json({
+            links: [
+                {
+                    rel: "lrdd",
+                    type: "application/json",
+                    template: "http://"+hostname+"/lrdd.json?uri={uri}"
+                },
+                {
+                    rel: "dialback",
+                    href: "http://"+hostname+"/api/dialback"
+                }
+            ]
+        });
+    });
+
+    app.get("/lrdd.json", function(req, res) {
+        var uri = req.query.uri,
+            parts = uri.split("@"),
+            username = parts[0],
+            hostname = parts[1];
+
+        res.json({
+            links: [
+                {
+                    rel: "dialback",
+                    href: "http://"+hostname+"/api/dialback"
+                }
+            ]
+        });
+    });
+
+    app.on("error", function(err) {
+        callback(err, null);
+    });
+
+    app.listen(port, hostname, function() {
+        callback(null, app);
+    });
+};
+
+suite.addBatch({
+    "When we set up the app": {
+        topic: function() {
+            var app,
+                callback = this.callback,
+                db = Databank.get(tc.driver, tc.params);
+
+            Step(
+                function() {
+                    db.connect({}, this);
+                },
+                function(err) {
+                    if (err) throw err;
+                    DatabankObject.bank = db;
+                    setupAppConfig({port: 80, hostname: "social.localhost", driver: "memory", params: {}}, this);
+                },
+                function(err, result) {
+                    if (err) throw err;
+                    app = result;
+                    tinyApp(80, "dialback.localhost", this);
+                },
+                function(err, dbapp) {
+                    var dialbackClient;
+                    if (err) {
+                        callback(err, null, null);
+                    } else {
+                        URLMaker.hostname = "dialback.localhost";
+                        callback(err, app, dbapp);
+                    }
+                }
+            );
+        },
+        teardown: function(app, dbapp) {
+            app.close();
+            dbapp.close();
+        },
+        "it works": function(err, app, dbapp) {
+            assert.ifError(err);
+        },
+        "and we ensure an invalid host": {
+            topic: function() {
+                var callback = this.callback;
+
+                Host.ensureHost("other.invalid",
+                                function(err, cred) {
+                                    if (err) {
+                                        callback(null);
+                                    } else {
+                                        callback(new Error("Unexpected success"));
+                                    }
+                                });
+            },
+            "it fails correctly": function(err) {
+                assert.ifError(err);
+            }
+        },
+        "and we ensure a valid host": {
+            topic: function() {
+                var callback = this.callback;
+
+                Host.ensureHost("social.localhost", callback);
+            },
+            "it works": function(err, host) {
+                assert.ifError(err);
+                assert.isObject(host);
+            },
+            "and we check its properties": function(err, host) {
+                assert.ifError(err);
+                assert.isObject(host);
+                assert.isString(host.hostname);
+                assert.isString(host.registration_endpoint);
+                assert.isString(host.request_token_endpoint);
+                assert.isString(host.access_token_endpoint);
+                assert.isString(host.authorization_endpoint);
+                assert.isString(host.whoami_endpoint);
+                assert.isNumber(host.created);
+                assert.isNumber(host.modified);
+            },
+            "and we ensure the same host again": {
+                topic: function(host) {
+                    var callback = this.callback;
+
+                    Host.ensureHost(host.hostname, function(err, dupe) {
+                        callback(err, dupe, host);
+                    });
+                },
+                "it works": function(err, dupe, host) {
+                    assert.ifError(err);
+                    assert.isObject(dupe);
+                    assert.isObject(host);
+                },
+                "and we check its properties": function(err, dupe, host) {
+                    assert.ifError(err);
+                    assert.isObject(host);
+                    assert.isObject(dupe);
+                    assert.deepEqual(dupe, host);
+                }
+            }
+        }
+    }
+});
+
+suite["export"](module);
