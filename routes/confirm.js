55c3510a21ff1b494b0ad3cceebce77fdf877795
Evan Prodromou <evan@status.net>
2013-02-15 16:10:30 -0500

    Better handling of confirm

--- routes/confirm.js
+++ routes/confirm.js
@@ -68,11 +68,16 @@ var confirm = function(req, res, next) {
             confirm.save(this.parallel());
         },
         function(err, res1, res2) {
+            if (err) throw err;
+            setPrincipal(req.session, user.profile, this);
+        },
+        function(err) {
             if (err) {
                 next(err);
             } else {
-                setPrincipal(req.session, user.profile, this);
-                res.render("confirmed", {page: {title: "Email address confirmed"}});
+                res.render("confirmed", {page: {title: "Email address confirmed"},
+                                         principalUser: user,
+                                         principal: user.profile});
             }
         }
     );
