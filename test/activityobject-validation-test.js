7eadcbf000fe22291a94ba9a707a559b501db22b
Evan Prodromou <evan@e14n.com>
2013-05-07 08:47:15 -0400

    Check that attachments only includes objects

--- test/activityobject-validation-test.js
+++ test/activityobject-validation-test.js
@@ -130,7 +130,7 @@ suite.addBatch({
             topic: function(ActivityObject) {
                 var callback = this.callback,
                     props = {
-                        id: "urn:uuid:015a8bd6-b706-11e2-b87b-c8f73398600c",
+                        id: "urn:uuid:c6df5d5c-b713-11e2-9369-2c8158efb9e9",
                         objectType: {
                             left: 1,
                             right: 2
@@ -155,9 +155,32 @@ suite.addBatch({
             topic: function(ActivityObject) {
                 var callback = this.callback,
                     props = {
-                        id: "urn:uuid:015a8bd6-b706-11e2-b87b-c8f73398600c",
+                        id: "urn:uuid:da0cea20-b713-11e2-8998-2c8158efb9e9",
                         objectType: "note",
                         attachments: 4
+                    };
+
+                ActivityObject.ensureObject(props, function(err, obj) {
+                    if (err && err instanceof TypeError) {
+                        callback(null);
+                    } else if (err) {
+                        callback(err);
+                    } else {
+                        callback(new Error("Unexpected success"));
+                    }
+                });
+            },
+            "it fails correctly": function(err) {
+                assert.ifError(err);
+            }
+        },
+        "and we try to ensureObject with a non-object in attachments property": {
+            topic: function(ActivityObject) {
+                var callback = this.callback,
+                    props = {
+                        id: "urn:uuid:e4168198-b713-11e2-add9-2c8158efb9e9",
+                        objectType: "note",
+                        attachments: [1, 2, 3, 4]
                     };
 
                 ActivityObject.ensureObject(props, function(err, obj) {
