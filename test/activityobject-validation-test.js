6493faec3f35fd4f86bf1cf3e371d6c8cab04679
Evan Prodromou <evan@e14n.com>
2013-05-07 09:22:21 -0400

    Validate that displayName is a string

--- test/activityobject-validation-test.js
+++ test/activityobject-validation-test.js
@@ -297,6 +297,32 @@ suite.addBatch({
             "it fails correctly": function(err) {
                 assert.ifError(err);
             }
+        },
+        "and we try to ensureObject with non-string displayName": {
+            topic: function(ActivityObject) {
+                var callback = this.callback,
+                    props = {
+                        id: "urn:uuid:ea3d349a-b718-11e2-a275-2c8158efb9e9",
+                        objectType: "person",
+                        displayName: {
+                            first: "Captain",
+                            last: "Marvel"
+                        }
+                    };
+
+                ActivityObject.ensureObject(props, function(err, obj) {
+                    if (err && err instanceof TypeError) {
+                        callback(null);
+                    } else if (err) {
+                        callback(err);
+                    } else {
+                        callback(new Error("Unexpected success"));
+                    }
+                });
+            },
+            "it fails correctly": function(err) {
+                assert.ifError(err);
+            }
         }
     }
 });
