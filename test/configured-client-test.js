262b95e1aa41f6f7cc66b84bf2cd1737565ef648
Evan Prodromou <evan@e14n.com>
2013-07-02 15:27:46 -0400

    Test for configured client

---
+++ test/configured-client-test.js
@@ -0,0 +1,52 @@
+// configured-client-test.js
+//
+// Test the config.clients array
+//
+// Copyright 2013, E14N https://e14n.com/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert"),
+    vows = require("vows"),
+    Step = require("step"),
+    _ = require("underscore"),
+    httputil = require("./lib/http"),
+    oauthutil = require("./lib/oauth"),
+    setupAppConfig = oauthutil.setupAppConfig;
+
+var CLIENT_ID_1 = "AAAAAAAAAAAAAAAAAAAA",
+    CLIENT_SECRET_1 = "BBBBBBBBBBBBBBBBBBBB";
+
+var suite = vows.describe("configured client ID");
+
+suite.addBatch({
+    "When we set up the app with a configured client": {
+        topic: function() {
+	    var config = {
+		clients: [{
+		    client_id: CLIENT_ID_1,
+		    client_secret: CLIENT_SECRET_1
+		}]
+	    };
+            setupAppConfig(config, this.callback);
+        },
+        teardown: function(app) {
+            app.close();
+        },
+        "it works": function(err, app) {
+            assert.ifError(err);
+        }
+    }
+});
+
+suite["export"](module);
