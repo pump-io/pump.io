695b373a9a151f74e333f0d75dc2eec225efd2c6
Evan Prodromou <evan@e14n.com>
2013-07-24 12:21:10 -0400

    defang HTML in displayName when added to titles

--- public/javascript/pump.js
+++ public/javascript/pump.js
@@ -560,7 +560,8 @@ if (!window.Pump) {
     };
 
     Pump.setTitle = function(title) {
-        $("title").html(title + " - " + Pump.config.site);
+        // We don't accept HTML in title or site name; just text
+        $("title").text(title + " - " + Pump.config.site);
     };
 
 })(window._, window.$, window.Backbone, window.Pump);
