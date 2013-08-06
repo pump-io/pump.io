3e905defa650c411e0fb9e66e907b305d2ce4de9
Evan Prodromou <evan@e14n.com>
2013-08-06 14:54:47 -0400

    Tests for full data on actor, object, in reply to

--- test/activity-content-test.js
+++ test/activity-content-test.js
@@ -124,26 +124,26 @@ suite.addBatch({
         },
         "and a person with no url posts a note":
         contentCheck({objectType: "person",
-                      id: "cbf1f0ca-2e10-11e2-8174-70f1a154e1aa",
+                      id: "urn:uuid:cbf1f0ca-2e10-11e2-8174-70f1a154e1aa",
                       displayName: "Jessie Belnap"},
                      "post",
                      {objectType: "note",
-                      id: "f4f859a0-2e10-11e2-9af6-70f1a154e1aa",
+                      id: "urn:uuid:f4f859a0-2e10-11e2-9af6-70f1a154e1aa",
                       content: "Hello, world."},
                      "Jessie Belnap posted a note"),
         "and a person with an url posts a note":
         contentCheck({objectType: "person",
-                      id: "e67c969c-2e11-11e2-86dc-70f1a154e1aa",
+                      id: "urn:uuid:e67c969c-2e11-11e2-86dc-70f1a154e1aa",
                       url: "http://malloryfellman.example/",
                       displayName: "Mallory Fellman"},
                      "post",
                      {objectType: "note",
-                      id: "a453c0fa-2e12-11e2-9214-70f1a154e1aa",
+                      id: "urn:uuid:a453c0fa-2e12-11e2-9214-70f1a154e1aa",
                       content: "Hello, world."},
                      "<a href='http://malloryfellman.example/'>Mallory Fellman</a> posted a note"),
         "and a person with no url plays a video":
         contentCheck({objectType: "person",
-                      id: "6958bee6-2e13-11e2-be89-70f1a154e1aa",
+                      id: "urn:uuid:6958bee6-2e13-11e2-be89-70f1a154e1aa",
                       displayName: "Mathew Penniman"},
                      "play",
                      {objectType: "video",
@@ -152,30 +152,101 @@ suite.addBatch({
                      "Mathew Penniman played <a href='http://www.youtube.com/watch?v=J---aiyznGQ'>Keyboard Cat</a>"),
         "and a person with no url closes a file":
         contentCheck({objectType: "person",
-                      id: "5abbfc3a-2e14-11e2-b27c-70f1a154e1aa",
+                      id: "urn:uuid:5abbfc3a-2e14-11e2-b27c-70f1a154e1aa",
                       displayName: "Clayton Barto"},
                      "close",
                      {objectType: "file",
-                      id: "8e5b739a-2e14-11e2-ab8e-70f1a154e1aa"},
+                      id: "urn:uuid:8e5b739a-2e14-11e2-ab8e-70f1a154e1aa"},
                      "Clayton Barto closed a file"),
         "and a person posts a comment in reply to an image":
         contentCheck({objectType: "person",
-                      id: "dc3c3572-2e14-11e2-91fc-70f1a154e1aa",
+                      id: "urn:uuid:dc3c3572-2e14-11e2-91fc-70f1a154e1aa",
                       displayName: "Lorrie Tynan"},
                      "post",
                      {objectType: "comment",
-                      id: "f58da61e-2e14-11e2-a207-70f1a154e1aa",
+                      id: "urn:uuid:f58da61e-2e14-11e2-a207-70f1a154e1aa",
                       inReplyTo: {
                           objectType: "image",
-                          id: "0d61105a-2e15-11e2-b6b4-70f1a154e1aa",
+                          id: "urn:uuid:0d61105a-2e15-11e2-b6b4-70f1a154e1aa",
                           author: {
                               displayName: "Karina Cosenza",
-                              id: "259e793c-2e15-11e2-b437-70f1a154e1aa",
+                              id: "urn:uuid:259e793c-2e15-11e2-b437-70f1a154e1aa",
                               objectType: "person"
                           }
                       }
                      },
-                     "Lorrie Tynan posted a comment in reply to an image")
+                     "Lorrie Tynan posted a comment in reply to an image"),
+        "and we create a person": {
+            topic: function(Activity) {
+                var Person = require("../lib/model/person").Person,
+                    callback = this.callback;
+                Person.create({id: "urn:uuid:3e2273f4-fec7-11e2-9db1-32b36b1a1850",
+                               displayName: "Endicott Pettibone"},
+                              function(err, person) {
+                                  callback(err);
+                              });
+            },
+            "it works": function(err) {
+                assert.ifError(err);
+            },
+            "and we post an activity without the full actor data": 
+            contentCheck({objectType: "person",
+                               id: "urn:uuid:3e2273f4-fec7-11e2-9db1-32b36b1a1850"},
+                         "like",
+                         {objectType: "image",
+                          id: "urn:uuid:9f535cb0-fec7-11e2-9637-32b36b1a1850",
+                          displayName: "La Giocanda"},
+                         "Endicott Pettibone liked La Giocanda.")
+        },
+        "and we create an image": {
+            topic: function(Activity) {
+                var Image = require("../lib/model/image").Image,
+                    callback = this.callback;
+                Image.create({id: "urn:uuid:1731d374-fec8-11e2-87ba-32b36b1a1850",
+                              displayName: "John the Baptist"},
+                              function(err, image) {
+                                  callback(err);
+                              });
+            },
+            "it works": function(err) {
+                assert.ifError(err);
+            },
+            "and we post an activity without the full object data": 
+            contentCheck({objectType: "person",
+                          id: "urn:uuid:6a8eab28-fec8-11e2-a39d-32b36b1a1850",
+                          displayName: "Catherine Hawk"},
+                         "like",
+                         {id: "urn:uuid:1731d374-fec8-11e2-87ba-32b36b1a1850",
+                          objectType: "image"},
+                         "Catherine Hawk liked John the Baptist.")
+        },
+        "and we create an article": {
+            topic: function(Activity) {
+                var Article = require("../lib/model/article").Article,
+                    callback = this.callback;
+                Article.create({id: "urn:uuid:af6ecc82-fec8-11e2-ac18-32b36b1a1850",
+                                displayName: "The End of History"},
+                               function(err, article) {
+                                   callback(err);
+                               });
+            },
+            "it works": function(err) {
+                assert.ifError(err);
+            },
+            "and we post an activity without the full inReplyTo data": 
+            contentCheck({objectType: "person",
+                          id: "urn:uuid:1f91eea4-fec9-11e2-8d0c-32b36b1a1850",
+                          displayName: "Zee Modem"},
+                         "post",
+                         {id: "urn:uuid:47a5e800-fec9-11e2-8af9-32b36b1a1850",
+                          objectType: "comment",
+                          inReplyTo: {
+                              id: "urn:uuid:af6ecc82-fec8-11e2-ac18-32b36b1a1850",
+                              objectType: "article"
+                          }
+                         },
+                         "Zee Modem posted a comment in reply to The End of History.")
+        }
     }
 });
 
