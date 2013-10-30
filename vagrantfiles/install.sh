de0e6427945731d8b836520889fd73888c59e033
Martin Higham <martin@ocastalabs.com>
2013-10-30 14:56:03 +0000

    Change npm install to use chris lea's packed version

--- vagrantfiles/install.sh
+++ vagrantfiles/install.sh
@@ -5,27 +5,16 @@ if ! hash mongod 2>/dev/null; then
     echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
 fi
 
-sudo aptitude update
-sudo aptitude install build-essential vim curl make mongodb-10gen graphicsmagick -y
-
 if ! hash npm 2>/dev/null; then
-    echo '# Added by install script for node.js and npm in 30s' >> ~/.bashrc
-    echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-    echo 'export NODE_PATH=$HOME/local/lib/node_modules' >> ~/.bashrc
-    . ~/.bashrc
-
-    mkdir -p ~/local
-    mkdir -p ~/Downloads/node-latest-install
-
-    cd ~/Downloads/node-latest-install
-    curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-
-    ./configure # if SSL support is not required, use --without-ssl
-    sudo make install
+    sudo aptitude update
+    sudo aptitude install build-essential vim curl make mongodb-10gen graphicsmagick python-software-properties -y
+    sudo add-apt-repository ppa:chris-lea/node.js -y
+    sudo aptitude update
+    sudo aptitude install nodejs -y
+    
     echo npm version `npm -v`
     echo node version `node -v`
 fi
-
 
 cd /vagrant
 sudo cp vagrantfiles/pump.io.json /etc/pump.io.json 
