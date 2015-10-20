fa4bc66bb18cbe1b31f029f125dafce84a16e687
James Michael DuPont <JamesMikeDuPont@gmail.com>
2015-10-20 06:35:24 -0400

    clean up

---
+++ setup.sh
@@ -0,0 +1,4 @@
+./bin/
+./bin/pump-register-app  -t test -s 127.0.0.1 -P 31337
+./bin/pump-register-user  -u test5 -p Test1234 -s 127.0.0.1 -P 31337
+./bin/pump-authorize -u test6 -s 127.0.0.1 -P 31337
