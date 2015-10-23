3e4a319405af6933905e8c10e8bc0e230f3e60cf
James Michael DuPont <JamesMikeDuPont@gmail.com>
2015-10-23 07:01:08 -0400

    update

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 ./bin/pump-register-app  -t test -s 127.0.0.1 -P 31337
 ./bin/pump-register-user  -u test5 -p Test1234 -s 127.0.0.1 -P 31337
 ./bin/pump-authorize -u test6 -s 127.0.0.1 -P 31337
+./bin/pump-follow -u test5  -s 127.0.0.1 -P 31337 -o h4ck3rm1k3@identi.ca
