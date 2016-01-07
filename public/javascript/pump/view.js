7f8d92ce2cd38f17d7afdd380aa4442c84a17b82
Roberto Guido <bob4job@gmail.com>
2016-01-07 02:07:27 +0100

    Add cancel button in comment's form
    Closes: #710

--- public/javascript/pump/view.js
+++ public/javascript/pump/view.js
@@ -1590,13 +1590,18 @@
         tagName: "div",
         className: "row comment-form",
         events: {
-            "submit .post-comment": "saveComment"
+            "submit .post-comment": "saveComment",
+            "click .close-btn": "cancelComment"
         },
         ready: function() {
             var view = this;
             view.$('textarea[name="content"]').wysihtml5({
                 customTemplates: Pump.wysihtml5Tmpl
             });
+        },
+        cancelComment: function() {
+            var view = this;
+            view.remove();
         },
         saveComment: function() {
             var view = this,
