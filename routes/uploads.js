09a87c64df893cc5bb4002746a644a6e5c10f39b
Alex Jordan <alex@strugee.net>
2016-07-18 14:38:27 -0700

    Fix another instance of connect.middleware.static.send
    Fixes #1174

--- routes/uploads.js
+++ routes/uploads.js
@@ -109,7 +109,7 @@ var uploadedFile = function(req, res, next) {
             } else if (!flag) {
                 next(new HTTPError("Not allowed", 403));
             } else {
-                send(req, res, next, {path: slug, root: req.app.config.uploaddir});
+                send(req, req.app.config.uploaddir + "/" +  slug).pipe(res);
             }
         }
     );
