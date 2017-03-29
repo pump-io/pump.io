7128ca1009a81f4a6be1b1da27eb4d6194cccad9
Alex Jordan <alex@strugee.net>
2017-03-29 11:49:46 -0400

    Revert "Correct pump.io.json.sample using `uploadsEnabled`"
    This reverts commit 6d421e6b38210786596e046e06025dffdc941773, since
    that accidentally introduced changes meant for a separate branch.

--- test/test-lib-oauth-test.js
+++ test/test-lib-oauth-test.js
@@ -22,8 +22,7 @@ var assert = require("assert"),
     http = require("http"),
     vows = require("vows"),
     Step = require("step"),
-    _ = require("underscore"),
-    apputil = require("./lib/app");
+    _ = require("underscore");
 
 var suite = vows.describe("user REST API");
 
@@ -34,6 +33,10 @@ suite.addBatch({
         },
         "it works": function(oauth) {
             assert.isObject(oauth);
+        },
+        "it has a setupApp() export": function(oauth) {
+            assert.isTrue(_(oauth).has("setupApp"));
+            assert.isFunction(oauth.setupApp);
         },
         "it has a newClient() export": function(oauth) {
             assert.isTrue(_(oauth).has("newClient"));
@@ -57,7 +60,7 @@ suite.addBatch({
         },
         "and we setup the app": {
             topic: function(oauth) {
-                apputil.setupApp(this.callback);
+                oauth.setupApp(this.callback);
             },
             "it works": function(err, app) {
                 assert.ifError(err);
