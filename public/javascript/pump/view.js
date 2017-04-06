1332bf07e69ca0a13971cc97d5f29b488a6c46c9
Camilo QS <vxcamiloxv@openmailbox.org>
2017-04-05 22:57:17 -0500

    Fix close user dropdown when link is clicked
    Closes: #1271

--- public/javascript/pump/view.js
+++ public/javascript/pump/view.js
@@ -540,7 +540,8 @@
         events: {
             "click #logout": "logout",
             "click #post-note-button": "postNoteModal",
-            "click #post-picture-button": "postPictureModal"
+            "click #post-picture-button": "postPictureModal",
+            "click #fat-menu .dropdown-menu li": "closeDropdown"
         },
         postNoteModal: function() {
             var view = this;
@@ -630,6 +631,10 @@
                 streams.notifications = view.minorStreamView.model;
             }
             return streams;
+        },
+        closeDropdown: function(e) {
+            e.preventDefault();
+            $("#profile-dropdown").dropdown("toggle");
         }
     });
 
