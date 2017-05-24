90fd13d519ce6b7bb91720f3aeb5510b654ab90b
Alex Jordan <alex@strugee.net>
2017-05-23 21:20:19 -0700

    Correct xss-blacklist-test copyright header

--- test/xss-blacklist-test.js
+++ test/xss-blacklist-test.js
@@ -2,7 +2,7 @@
 //
 // Test the XSS blacklist middleware
 //
-// Copyright 2012, E14N https://e14n.com/
+// Copyright 2016 Alex Jordan <alex@strugee.net>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
