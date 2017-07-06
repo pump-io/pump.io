4686c060717fe12110b9c7d8fefab1f1f62f4eec
AJ Jordan <alex@strugee.net>
2017-07-06 03:25:21 -0500

    Explicitly set view.options for Backbone Views
    Backbone 1.1.0 removed this feature from core.

--- public/javascript/pump/view.js
+++ public/javascript/pump/view.js
@@ -48,6 +48,8 @@
         initialize: function(options) {
             var view = this;
 
+            view.options = options || {};
+
             if (_.has(view, "model") && _.isObject(view.model)) {
                 view.listenTo(view.model, "change", function(model, options) {
                     Pump.debug("Re-rendering " + view.templateName + " #" + view.cid + " based on change to " + view.model.id);
@@ -1887,6 +1889,8 @@
         modelName: "profile",
         parts: ["profile-responses"],
         initialize: function(options) {
+            this.options = options || {};
+
             Pump.debug("Initializing profile-block #" + this.cid);
             Pump.PersonView.prototype.initialize.apply(this);
         }
@@ -3016,6 +3020,8 @@
 
     Pump.BodyView = Backbone.View.extend({
         initialize: function(options) {
+            this.options = options || {};
+
             _.bindAll(this, "navigateToHref");
         },
         el: "body",
@@ -3354,6 +3360,9 @@
         },
         initialize: function(options) {
             var view = this;
+
+            view.options = options || {};
+
             if (options.parent) {
                 view.parent = options.parent;
             }
