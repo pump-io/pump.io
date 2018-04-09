ce38869242038acb021d6be606b38403d4667526
Evan Prodromou <evan@prodromou.name>
2018-04-09 12:29:37 -0400

    Use OAuth 2.0 for ActivityPub outbox get test

--- test/activitypub-get-outbox-test.js
+++ test/activitypub-get-outbox-test.js
@@ -1,8 +1,9 @@
 // activitypub-outbox-test.js
 //
-// Test the AS2 conversion module
+// Test the ActivityPub outbox endpoint
 //
 // Copyright 2017 AJ Jordan <alex@strugee.net>
+// Copyright 2018 E14N <evan@e14n.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +19,8 @@
 
 "use strict";
 
-var assert = require("assert"),
+var _ = require("lodash"),
+    assert = require("assert"),
     vows = require("vows"),
     uuid = require("uuid"),
     apputil = require("./lib/app"),
@@ -26,44 +28,88 @@ var assert = require("assert"),
     oauthutil = require("./lib/oauth"),
     newCredentials = oauthutil.newCredentials;
 
-var suite = vows.describe("ActivityPub outbox");
+var tc = require("./config.json");
 
 var AS2_MIME_TYPE = "application/ld+json; profile=\"https://www.w3.org/ns/activitystreams\"";
 var AS2_CONTEXT = "https://www.w3.org/ns/activitystreams";
 
+var user = tc.users[3];
+var client = tc.clients[0];
+
+var assertValidAS2Activity = function(act) {
+    assert.isObject(act);
+    assert.isString(act.id);
+    assert(_.isString(act.name) || _.isString(act.summary));
+    assert.isObject(act.actor);
+    assert.isString(act.actor.id);
+    assert.isString(act.actor.name);
+    assert.isString(act.actor.type);
+    assert.isObject(act.object);
+    assert.isString(act.object.id);
+    assert.isString(act.object.name);
+    assert.isString(act.object.type);
+    assert.isString(act.published);
+};
+
+process.on('uncaughtException', function(err) {
+    console.error(err);
+    process.exit(-1);
+});
+
+var suite = vows.describe("ActivityPub outbox");
+
 suite.addBatch(apputil.withAppSetup({
-  "and we get new credentials": {
-      topic: function() {
-          newCredentials("macdonald", "the|old|flag", this.callback);
-      },
-      "it works": function(err, cred) {
-          assert.ifError(err);
-      },
-      "and we request an identity URL for ActivityPub": {
-        topic(cred) {
-          var cb = this.callback;
-          var headers = {
-            Accept: AS2_MIME_TYPE
-          };
-          httputil.getJSON("http://localhost:4815/macdonald", cred, headers, function(err, body, response) {
-              cb(err, response, body);
-          });
-        },
-        "it works": function(err, res, body) {
-          assert.ifError(err);
-        },
-        "it has an outbox link": function(err, res, body) {
-          assert.isObject(body);
-          assert.isString(body.outbox);
-        },
-        "and we request the outbox": {
-          topic: function(res, body, cred) {
+    "and we request an identity URL for ActivityPub": {
+        topic() {
             var cb = this.callback;
             var headers = {
-              Accept: AS2_MIME_TYPE
+                Accept: AS2_MIME_TYPE,
+                Authorization: "Bearer " + user.tokens[0].token
             };
-            httputil.getJSON(body.outbox, cred, headers, function(err, body, response) {
-                cb(err, response, body);
+            var url = "http://localhost:4815/" + user.nickname;
+            httputil.get(url, headers, function(err, response, body) {
+                if (err) {
+                    cb(err);
+                } else if (response.statusCode !== 200) {
+                    cb(new Error("Unexpected status code: " + response.statusCode));
+                } else {
+                    var data = null;
+                    try {
+                        data = JSON.parse(body);
+                    } catch (e) {
+                        return cb(e);
+                    }
+                    return cb(null, response, data);
+                }
+            });
+        },
+        "it works": function(err, res, body) {
+            assert.ifError(err);
+            assert.isObject(res);
+            assert.isObject(body);
+            assert.isString(body.outbox);
+        },
+        "and we request the outbox link": {
+          topic: function(ignore, actor) {
+            var cb = this.callback;
+            var headers = {
+              Accept: AS2_MIME_TYPE,
+              Authorization: "Bearer " + user.tokens[0].token
+            };
+            httputil.get(actor.outbox, headers, function(err, response, body) {
+                if (err) {
+                    cb(err);
+                } else if (response.statusCode !== 200) {
+                    cb(new Error("Unexpected status code: " + response.statusCode));
+                } else {
+                    var data = null;
+                    try {
+                        data = JSON.parse(body);
+                    } catch (e) {
+                        return cb(e);
+                    }
+                    return cb(null, response, data);
+                }
             });
           },
           "it works": function(err, res, body) {
@@ -77,16 +123,14 @@ suite.addBatch(apputil.withAppSetup({
             assert.isObject(body);
             assert.isString(body.type);
             assert.equal(body.type, "OrderedCollection");
+            // Because this is a fake user created during config, the
+            // default list-creation activities aren't here.
             assert.isNumber(body.totalItems);
-            assert.greater(body.totalItems, 0);
-            assert.isObject(body.first);
-            assert.isString(body.first.type);
-            assert.equal(body.first.type, "OrderedCollectionPage");
-            assert.isArray(body.first.orderedItems);
-            // XXX: check that there is an object in items
-            // XXX: check that that object is an activity
+            assert.equal(body.totalItems, 0);
+            assert.notIncludes(body, "first");
+            assert.notIncludes(body, "items");
+            assert.notIncludes(body, "orderedItems");
           }
         }
-      }
     }
   })).export(module);
