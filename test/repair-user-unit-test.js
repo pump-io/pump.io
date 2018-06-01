26a5e4307794661e1b1a397f0cd295013fbf6dd7
AJ Jordan <alex@strugee.net>
2018-06-01 02:45:30 -0700

    Improve a Vows batch description

--- test/repair-user-unit-test.js
+++ test/repair-user-unit-test.js
@@ -39,8 +39,7 @@ var suite = vows.describe("automatic repair of user profile");
 var tc = JSON.parse(fs.readFileSync(path.resolve(__dirname, "config.json")));
 
 suite.addBatch({
-
-    "When we set up the app": {
+    "When we initialize the environment": {
         topic: function() {
 
             var cb = this.callback;
