d541438287eb9900b4bfbdd82a3af9595e73412a
Distopico <distopico@riseup.net>
2020-04-09 14:27:13 -0500

    Add support to Node 10,12 and 13 drop support to 4 >= 9

--- util/docker-candidacy.sh
+++ util/docker-candidacy.sh
@@ -6,15 +6,11 @@ cd $(dirname $0)
 
 TYPE=$(./determine-release-type.sh)
 
-check_right_node_version() {
-	test $(node -p "require('process').versions.node.split('.')[0]") = 8
-}
-
 if [ $TRAVIS_EVENT_TYPE = cron ]; then
-	check_right_node_version && exit 0
+	exit 0
 # Don't wait for the cronjob if it's a non-alpha
 elif [ $TYPE = release -o $TYPE = beta ]; then
-	check_right_node_version && exit 0
+	exit 0
 fi
 
 exit 1
